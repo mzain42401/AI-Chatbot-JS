4d797e534e9251b3088bf9e0c69d2a1d172c6cee
m zain ali <mzain42401@gmail.com>
2024-01-22 19:30:47 +0500

    practice

---
+++ app.js
@@ -0,0 +1,48 @@
+// alert("hello")
+
+// varibales
+
+// var , let , const
+
+
+
+// var
+
+// var a=10;
+
+
+// var a=20;
+// console.log(a);
+
+
+// var a=10;
+// console.log(a);
+
+
+// var a=49;
+// console.log(a);
+
+
+// let a=10;
+//  a=20;
+// console.log(a);
+
+
+// const a=3.13
+// a=8
+// console.log(a);
+
+
+// number , string , boolean 
+   
+
+// var a= 10; //number
+// var b= "huzefa" //string
+// var c= true // boolean 
+// var d=[98,76,56] //array
+// var e={
+//     name:"huzeaf",
+//     age: 18,
+//     maried:false
+// }
+// console.table(d);
